d72a5c495996bc2879c40280901507f6bf0c0127
Edgar Briandt <ebriandt_6243@email.ric.edu>
2024-04-19 10:21:50 -0400

    toggles galore :D

--- src/app/drop-down-dates/drop-down-dates.component.ts
+++ src/app/drop-down-dates/drop-down-dates.component.ts
@@ -12,10 +12,16 @@ import { Observable, catchError, tap, throwError } from 'rxjs';
 })
 export class DropDownDatesComponent implements OnInit {
   personData: any = [];
-  uniqueDates: any[] = [];
-  selectedDate: string | null = null;
-  filteredData: any[] = [];
+  // uniqueDates: any[] = [];
+  // selectedDate: string | null = null;
+  // filteredData: any[] = [];
   dateToggleStates: { [key: string]: boolean } = {};
+  isTotalActive: boolean = false;
+  isPriorityListActive: boolean = false;
+  isNormalListActive: boolean = false;
+
+  isPriorityData: any[] = [];
+  isNormalData: any[] = [];
 
   constructor(private _http: HttpClient) {}
 
@@ -23,7 +29,8 @@ export class DropDownDatesComponent implements OnInit {
     this.getPersonData().subscribe(
       (data) => {
         console.log('Data received:', data);
-        this.extractUniqueDates();
+        // this.extractUniqueDates();
+        this.splitPersonData();
       },
       (error) => {
         console.error('Error fetching data:', error);
@@ -31,37 +38,96 @@ export class DropDownDatesComponent implements OnInit {
     );
   }
 
-  extractUniqueDates(): void {
-    this.uniqueDates = [
-      ...new Set(this.personData.map((person: any) => person.birthdate)),
-    ];
-    this.uniqueDates.forEach((date) => {
-      this.dateToggleStates[date] = false; // Initially set all toggles to false (hide)
-    });
+  toggleIsTotalActive(): void {
+    this.isTotalActive = !this.isTotalActive;
   }
 
-  filterDataByDate(date: string): any[] {
-    return this.personData.filter((person: any) => person.birthdate === date);
-  }
-
-  toggleDate(date: string): void {
-    if (this.dateToggleStates[date]) {
-      this.dateToggleStates[date] = false;
+  togglePriorityList(): void {
+    if (this.isPriorityListActive) {
+      this.isPriorityListActive = false;
     } else {
-      Object.keys(this.dateToggleStates).forEach((key) => {
-        this.dateToggleStates[key] = false;
-      });
-      this.dateToggleStates[date] = true;
+      this.isPriorityListActive = true;
+      this.isNormalListActive = false;
     }
   }
 
-  onDateSelected(event: any): void {
-    const selectedDate = event.target.value;
-    this.selectedDate = selectedDate;
-    this.filteredData = this.personData.filter(
-      (person: any) => person.birthdate === selectedDate
+  toggleNormalList(): void {
+    if (this.isNormalListActive) {
+      this.isNormalListActive = false;
+    } else {
+      this.isNormalListActive = true;
+      this.isPriorityListActive = false;
+    }
+  }
+
+  getTotalPersons(): number {
+    // let total = 0;
+    // this.uniqueDates.forEach((date) => {
+    //   total += this.filterDataByDate(date).length;
+    // });
+    // return total;
+
+    return this.personData.length;
+  }
+
+  // extractUniqueDates(): void {
+  //   this.uniqueDates = [
+  //     ...new Set(this.personData.map((person: any) => person.birthdate)),
+  //   ];
+  //   this.uniqueDates.forEach((date) => {
+  //     this.dateToggleStates[date] = false; // Initially set all toggles to false (hide)
+  //   });
+  // }
+
+  // filterDataByDate(date: string): any[] {
+  //   return this.personData.filter((person: any, index: number, self: any[]) => {
+  //     return (
+  //       person.birthdate === date &&
+  //       self.findIndex((p: any) => p.id === person.id) === index
+  //     );
+  //   });
+  // }
+
+  toggleDate(date: string): void {
+    this.dateToggleStates[date] = !this.dateToggleStates[date];
+    if (this.dateToggleStates[date]) {
+      Object.keys(this.dateToggleStates).forEach((key) => {
+        if (key !== date) {
+          this.dateToggleStates[key] = false;
+        }
+      });
+    }
+  }
+
+  filterPriorityDataByDate(date: string): any[] {
+    return this.isPriorityData.filter(
+      (person: any) => person.birthdate === date
     );
   }
+
+  get uniquePriorityDates(): string[] {
+    return [
+      ...new Set(this.isPriorityData.map((person: any) => person.birthdate)),
+    ];
+  }
+
+  filterNormalDataByDate(date: string): any[] {
+    return this.isNormalData.filter((person: any) => person.birthdate === date);
+  }
+
+  get uniqueNormalDates(): string[] {
+    return [
+      ...new Set(this.isNormalData.map((person: any) => person.birthdate)),
+    ];
+  }
+
+  // onDateSelected(event: any): void {
+  //   const selectedDate = event.target.value;
+  //   this.selectedDate = selectedDate;
+  //   this.filteredData = this.personData.filter(
+  //     (person: any) => person.birthdate === selectedDate
+  //   );
+  // }
 
   getPersonData(): Observable<any[]> {
     const url = 'http://localhost:3000/person';
@@ -76,5 +142,17 @@ export class DropDownDatesComponent implements OnInit {
         return throwError(() => new Error('Error fetching data'));
       })
     );
+  }
+
+  splitPersonData(): void {
+    this.isPriorityData = this.personData.filter(
+      (person: any) => person.priority
+    );
+    this.isNormalData = this.personData.filter(
+      (person: any) => !person.priority
+    );
+
+    console.log(this.isPriorityData.length);
+    console.log(this.isNormalData.length);
   }
 }
